a25b6e703c8d3eb0f191fca4519f036a04b7e60a
sowjanyapullabhatla <Sowjanyakruthii@gmail.com>
2025-10-02 17:49:06 -0500

    system internal first push to git

---
+++ System.Internals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Internals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

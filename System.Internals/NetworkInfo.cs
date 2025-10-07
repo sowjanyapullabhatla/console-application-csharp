e2772346ae1736359e6935f7199960fb6edb5ac0
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-10-07 18:21:39 -0500

    i have created a class to get the network details.

---
+++ System.Internals/NetworkInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Internals
+{
+    public class NetworkInfo
+    {
+        public void DisplayNetworkDetails()
+        {
+            string hostName = Dns.GetHostName(); // Get host name
+            Console.WriteLine($"Host Name: {hostName}");
+
+            // Get IP addresses
+            IPAddress[] addresses = Dns.GetHostAddresses(hostName);
+
+            Console.WriteLine("IP Addresses:");
+            foreach (IPAddress address in addresses)
+            {
+                // Display only IPv4 addresses (ignore IPv6)
+                if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine($"- {address}");
+                }
+            }
+        }
+    }
+}

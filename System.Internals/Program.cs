880e2bb0cc72b4549e5c5bcade486feaa2ee18bf
Kiran97685 <kiran.kurella97685@gmail.com>
2025-10-07 18:24:38 -0500

    My OS Details

--- System.Internals/Program.cs
+++ System.Internals/Program.cs
@@ -6,6 +6,15 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("welcome to programing world!");
+            // Create an object of OSdetails
+            OSdetails myOS = new OSdetails();
+
+            // Call the method to display OS info
+            myOS.DisplayOSDetails();
+
+            // Keep the console open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
